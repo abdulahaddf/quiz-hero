535f70a4e06e409150a2388e11ccbda0f1316a8c
abdulahaddf <abdulahad.df1@gmail.com>
2023-03-16 02:55:59 +0600

    final

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const loadQuiz = async () => {
 const displayQuiz = (data) => {
   console.log(data);
   if (!data) {
-    // quizContainer.innerHTML = ""; 
+    // quizContainer.innerHTML = "";
     return;
   }
   console.log(quizContainer.innerHTML);
@@ -141,24 +141,22 @@ document.querySelector("#submit").addEventListener("click", () => {
   let x = setTimeout(() => {
     showAnswers(answers);
     displayResult.innerHTML = `<div
-    class="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center border-2 rounded-tr-[50%] rounded-bl-[50%]"
-  >
+    class="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center border-2 rounded-tr-[50%] rounded-bl-[50%]">
     <h3 class="text-xl ${grade.color}">${grade.status}</h3>
     <h1 class="text-3xl font-bold my-2">
       ${totalMark}<span class="text-slate-800">/60</span>
     </h1>
     <p class="text-sm flex justify-center items-center gap-2">
       Total Time: <span class="text-xl text-orange-500">${timeTaken.innerText.replace(
-        "sec",
-        ""
-      )}<span class="text-xs">sec</span></span>
+      "sec",
+      ""
+    )}<span class="text-xs">sec</span></span>
     </p>
   </div>
   
   <button onclick="location.reload();" class="bg-green-600 text-white w-full py-2 rounded mt-16">Restart</button>
-  ${
-    storage
-? `<div class="mt-5">
+  ${storage
+        ? `<div class="mt-5">
       <h1 class="text-center">Previous Submissions <button class="text-blue-800 text-xs" onclick={localStorage.clear();location.reload()}>Clear History</button></h1>
     <div
     class="flex justify-between items-center border rounded p-2 my-2 shadow-sm font-medium">
@@ -167,18 +165,18 @@ document.querySelector("#submit").addEventListener("click", () => {
     <div>Time</div>
     </div>
     ${storage
-      ?.reverse()
-      ?.map(
-        (item) => `<div
+          ?.reverse()
+          ?.map(
+            (item) => `<div
       class="flex justify-between items-center border rounded p-2 my-2 shadow-sm">
       <div>${item.marks}/60</div>
       <div>${item.status}</div>
       <div>${item.examTime}</div>
       </div>`
-      )
-      ?.join("")}`
-      : ""
-  }
+          )
+          ?.join("")}`
+        : ""
+      }
   </div>
   `;
 
